2e32ef7b8fb2a0cff977c96f566d832f8de4d1e3
Kate Park <kate.of.park@gmail.com>
2017-05-14 15:33:12 -0700

    create train - in progress

--- baseline.py
+++ baseline.py
@@ -1,1 +1,10 @@
 from sklearn.linear_model import LogisticRegression
+import util
+import random
+import collections
+import math
+import sys
+import numpy as np
+
+
+class Regression:
---
+++ createbaselinetrain.py
@@ -0,0 +1,5 @@
+
+def buildTrainSet(self):
+	with open('data/switchboardsampletranscript') as input:
+		for line in input:
+			print line

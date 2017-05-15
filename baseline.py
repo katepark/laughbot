65f582d201b462b973d3c1c64ac74f31c72c38b6
Annie Hu <anniehu@stanford.edu>
2017-05-15 16:29:57 -0700

    logreg language model workin on small sample

--- baseline.py
+++ baseline.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import precision_recall_fscore_support
 import util
 import random
 import collections
@@ -8,19 +9,19 @@ import sys
 import numpy as np
 
 
-def fitModel(trainExamples):
+def fitModel(examples, vocab=None):
         corpus = []
-        for x,y in trainExamples:
+        for x,y in examples:
             corpus.append(x)
-
-        vectorizer = CountVectorizer(ngram_range=(1, 2),token_pattern=r'\b\w+\b', min_df=1)
+        vectorizer = CountVectorizer(vocabulary=vocab, ngram_range=(1, 2),token_pattern=r'\b\w+\b', min_df=1)
         X = vectorizer.fit_transform(corpus)
-        analyze = vectorizer.build_analyzer()
+        # analyze = vectorizer.build_analyzer()
         fullfeature = X.toarray()
         print 'SHAPE', len(fullfeature), len(fullfeature[0])
-        return vectorizer
+        # return vectorizer
+        return fullfeature, vectorizer.vocabulary_
 
-def featureExtractor(punchline, vectorizer):
+def featureExtractor(punchline, vocab):
         # print vectorizer.transform(punchline).toarray()
         feature = vectorizer.transform(punchline).toarray()
         # print 'SHAPE INDIV', len(feature), len(feature[0])
@@ -28,37 +29,41 @@ def featureExtractor(punchline, vectorizer):
     	
 def learnPredictor(trainExamples, valExamples, testExamples):
     	print 'BEGIN: GENERATE TRAIN'
-        vectorizer = fitModel(trainExamples)
+        trainFeatures, vocabulary = fitModel(trainExamples)
         trainX = []
     	trainY = []
     	for x,y in trainExamples:
-    		phi = featureExtractor(x, vectorizer)
-    		trainX.append(phi)
-    		trainY.append(y)
-    	numFeatures = len(trainX[0])
-        # print trainX
-        # print trainY
-    	# trainX = np.reshape(trainX, (len(trainX), numFeatures))
-    	# print 'END: GENERATE TRAIN'
-    	# print 'BEGIN: GENERATE TEST'
+    		# phi = featureExtractor(x, vectorizer)
+            phi = trainFeatures[len(trainX)]
+            trainX.append(phi)
+            trainY.append(y)
 
-    	# textX = []
-    	# testY = []
-    	# for x,y in testExamples:
-    	# 	phi = self.featureExtractor(x)
-    	# 	testX.append(phi)
-    	# 	testY.append(y)
-    	# print 'END: GENERATE TEST'
+    	print 'END: GENERATE TRAIN'
+    	print 'BEGIN: GENERATE TEST'
+        testFeatures, _ = fitModel(testExamples, vocab=vocabulary)
+    	testX = []
+    	testY = []
+    	for x,y in testExamples:
+    		phi = testFeatures[len(testX)]
+    		testX.append(phi)
+    		testY.append(y)
+    	print 'END: GENERATE TEST'
     	# testX = np.reshape(testX, (len(testX), numFeatures))
     	# print "Size: ", len(testX), len(testX[0])
 
     	# # val
         
-     #    print "BEGIN: TRAINING"
-        regr = LogisticRegression().fit(trainX, trainY)
-     #    print "END: TRAINING"
-        print "TRAIN score:", regr.score(trainX, trainY)
-     #    print "TEST score:", regr.score(testX, testY)
+        print "BEGIN: TRAINING"
+        regr = LogisticRegression()
+        regr.fit(trainX, trainY)
+        print "END: TRAINING"
+        trainPredict = regr.predict(trainX)
+        testPredict = regr.predict(testX)
+
+        precision,recall,fscore,support = precision_recall_fscore_support(trainY, trainPredict, average='binary')
+        print "TRAIN scores:\nPrecision:%f\nRecall:%f\nF1:%f" % (precision, recall, fscore)
+        precision,recall,fscore,support = precision_recall_fscore_support(testY, testPredict, average='binary')
+        print "TEST scores:\nPrecision:%f\nRecall:%f\nF1:%f" % (precision, recall, fscore)
 
 
 

e406be2bb238da44dca22ab0dd5f83b431478e67
Kate Park <kate.of.park@gmail.com>
2017-05-14 21:44:49 -0700

    create train test and val works

--- baseline.py
+++ baseline.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
 import util
 import random
 import collections
@@ -8,3 +9,34 @@ import numpy as np
 
 
 class Regression:
+
+	def featureExtractor(self, punchline):
+	
+	def learnPredictor(self, trainExamples, testExamples):
+		print 'BEGIN: GENERATE TRAIN'
+		trainX = []
+		trainY = []
+		for x,y in trainExamples:
+			phi = self.featureExtractor(x)
+			trainX.append(phi)
+			trainY.append(y)
+		print 'END: GENERATE TRAIN'
+		
+		print 'BEGIN: GENERATE TEST'
+		testY = []
+		textX = []
+		for x,y in testExamples:
+			phi = self.featureExtractor(x)
+			testX.append(phi)
+			testY.append(y)
+		print 'END: GENERATE TEST'
+        
+        print "BEGIN: TRAINING"
+		regr = LogisticRegression().fit(trainX, trainY)
+        print "END: TRAINING"
+
+
+
+
+trainExamples = util.readExamples('switchboardsample.train')
+testExamples = util.readExamples('switchboardsample.test')
--- createbaselinetrain.py
+++ createbaselinetrain.py
@@ -1,5 +1,39 @@
+import random
+import os 
+import glob
 
-def buildTrainSet(self):
-	with open('data/switchboardsampletranscript') as input:
-		for line in input:
-			print line
+def buildTrainSet():
+	trainFile = open('switchboardsample.train', 'w')
+	validationFile = open('switchboardsample.val', 'w')
+	testFile = open('switchboardsample.test', 'w')
+	print os.getcwd()
+	# iterate through all files in data
+	for inputname in glob.iglob(os.getcwd() + '/data/*'):
+		with open(inputname) as input:
+			lines = input.readlines()
+			for i in range(1, len(lines)):
+				line = lines[i]
+				words = line.split(' ')
+				if '<Laughter>' in words:
+					# TODO DECIDE: Strip <laughter> from punchlines before train??
+					# punchline = stripLaughter(lines[i-1].split(' '))
+					# punchline = ' '.join(punchline) # prevLine is punchline
+					punchline = '1 ' + lines[i-1]
+					classifiedLine = punchline
+				else:
+					classifiedLine = '0 ' + lines[i-1]
+				if random.random() < 0.8:
+					trainFile.write(classifiedLine)
+				elif random.random() < 0.9:
+					validationFile.write(classifiedLine)
+				else:
+					testFile.write(classifiedLine)
+				print classifiedLine
+
+
+def stripLaughter(words):
+	strippedLine = [x for x in words if (x.find('aughter>') < 0)]
+	return strippedLine
+
+
+buildTrainSet()

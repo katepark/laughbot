07672ec7518cd82082240062dbb3756a972928fc
Kate Park <kate.of.park@gmail.com>
2017-05-15 20:56:53 -0700

    read in sw data

--- createbaselinetrain.py
+++ createbaselinetrain.py
@@ -2,6 +2,8 @@ import random
 import os 
 import glob
 
+laughterregex = '[\[<][Ll]aughter[\]>].?'
+
 def buildTrainSet():
 	trainFile = open('switchboardsample.train', 'w')
 	validationFile = open('switchboardsample.val', 'w')
@@ -9,26 +11,35 @@ def buildTrainSet():
 	print os.getcwd()
 	# iterate through all files in data
 	for inputname in glob.iglob(os.getcwd() + '/data/*'):
+		header_found = False
 		with open(inputname) as input:
-			lines = input.readlines()
-			for i in range(1, len(lines)):
+			alllines = input.read().splitlines()
+			lines = [x for x in alllines if x != '']
+
+			# TODO: skip over header, start at line 18
+			for i in range(18, len(lines)):
 				line = lines[i]
-				words = line.split(' ')
-				if '<Laughter>' in words: # [Laughter], [laughter], with punctuation
-					# TODO DECIDE: Strip <laughter> from punchlines before train??
-					# punchline = stripLaughter(lines[i-1].split(' '))
-					# punchline = ' '.join(punchline) # prevLine is punchline
-					punchline = '1 ' + lines[i-1]
-					classifiedLine = punchline
-				else:
-					classifiedLine = '0 ' + lines[i-1]
-				if random.random() < 0.8:
-					trainFile.write(classifiedLine)
-				elif random.random() < 0.9:
-					validationFile.write(classifiedLine)
-				else:
-					testFile.write(classifiedLine)
-				print classifiedLine
+				if line != '':
+					print line 
+					words = line.split(' ')
+					# TODO: REGEX
+					if '[Laughter]' in words or '[Laughter].' in words or '[Laughter],' in words: # [Laughter], [laughter], with punctuation
+						# TODO DECIDE: Strip <laughter> from punchlines before train??
+						# punchline = stripLaughter(lines[i-1].split(' '))
+						# punchline = ' '.join(punchline) # prevLine is punchline
+						punchline = lines[i-1]
+						punchline = '1 ' + punchline + '\n'
+						classifiedLine = punchline
+					else:
+						punchline = lines[i-1]
+						classifiedLine = '0 ' + punchline + '\n'
+					if random.random() < 0.8:
+						trainFile.write(classifiedLine)
+					elif random.random() < 0.9:
+						validationFile.write(classifiedLine)
+					else:
+						testFile.write(classifiedLine)
+					# 	print classifiedLine
 
 
 def stripLaughter(words):

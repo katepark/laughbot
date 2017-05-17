9aabffe1e0b4c814d9ed46168ae8d098525828e7
Kate Park <kate.of.park@gmail.com>
2017-05-17 00:05:00 -0700

    frequent ngrams

--- baseline.py
+++ baseline.py
@@ -9,27 +9,38 @@ import sys
 import numpy as np
 
 
-def fitModel(examples, vocab=None):
+ngram_threshold = 10
+# 8 357143
+# 10 0.482759
+
+def fitModel(examples, vocab=None, frequent_ngram_col_idx=None):
         corpus = []
         for x,y in examples:
             corpus.append(x)
         vectorizer = CountVectorizer(vocabulary=vocab, ngram_range=(1, 3),token_pattern=r'\b\w+\b', min_df=1)
         X = vectorizer.fit_transform(corpus)
+        
         # analyze = vectorizer.build_analyzer()
         fullfeature = X.toarray()
-        print 'SHAPE', len(fullfeature), len(fullfeature[0])
-        # return vectorizer
-        return fullfeature, vectorizer.vocabulary_
 
-def featureExtractor(punchline, vocab):
-        # print vectorizer.transform(punchline).toarray()
-        feature = vectorizer.transform(punchline).toarray()
-        # print 'SHAPE INDIV', len(feature), len(feature[0])
-        return vectorizer.transform(punchline).toarray()
+        print 'SHAPE', len(fullfeature), len(fullfeature[0])
+
+        if not frequent_ngram_col_idx:
+            frequent_ngram_col_idx = []
+            for i in range(fullfeature.shape[1]):
+                if sum(fullfeature[:,i]) > ngram_threshold:
+                    frequent_ngram_col_idx.append(i)
+
+        fullfeature = fullfeature[:, frequent_ngram_col_idx]
+
+        print 'NEW SHAPE', len(fullfeature), len(fullfeature[0])
+        # return vectorizer
+        return fullfeature, vectorizer.vocabulary_, frequent_ngram_col_idx
+
     	
 def learnPredictor(trainExamples, valExamples, testExamples):
     	print 'BEGIN: GENERATE TRAIN'
-        trainFeatures, vocabulary = fitModel(trainExamples)
+        trainFeatures, vocabulary, freq_col_idx = fitModel(trainExamples)
         trainX = []
     	trainY = []
     	for x,y in trainExamples:
@@ -40,7 +51,7 @@ def learnPredictor(trainExamples, valExamples, testExamples):
 
     	print 'END: GENERATE TRAIN'
     	print 'BEGIN: GENERATE TEST'
-        testFeatures, _ = fitModel(testExamples, vocab=vocabulary)
+        testFeatures, _, freq_col_idx_test = fitModel(testExamples, vocab=vocabulary, frequent_ngram_col_idx=freq_col_idx)
     	testX = []
     	testY = []
     	for x,y in testExamples:

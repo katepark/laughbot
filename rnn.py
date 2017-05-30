8d96c2646e168d0ba4ff1ef0ae5692e5daa2f8ba
Natalie Muenster <ncm000@stanford.edu>
2017-05-30 10:04:15 -0700

    Added code for rnn functionality, currently trying to clean up errors.

--- rnn.py
+++ rnn.py
@@ -14,7 +14,7 @@ import tensorflow as tf
 import numpy as np
 from six.moves import xrange as range
 
-from utils import *
+from rnn_utils import *
 import pdb
 from time import gmtime, strftime
 
@@ -55,18 +55,6 @@ class CTCModel():
         inputs_placeholder: Input placeholder tensor of shape (None, None, num_final_features), type tf.float32
         targets_placeholder: Sparse placeholder, type tf.int32. You don't need to specify shape dimension.
         seq_lens_placeholder: Sequence length placeholder tensor of shape (None), type tf.int32
-
-        TODO: Add these placeholders to self as the instance variables
-            self.inputs_placeholder
-            self.targets_placeholder
-            self.seq_lens_placeholder
-
-        HINTS:
-            - Use tf.sparse_placeholder(tf.int32) for targets_placeholder. This is required by TF's ctc_loss op. 
-            - Inputs is of shape [batch_size, max_timesteps, num_final_features], but we allow flexible sizes for
-              batch_size and max_timesteps (hence the shape definition as [None, None, num_final_features]. 
-
-        (Don't change the variable names)
         """
         inputs_placeholder = None
         targets_placeholder = None
@@ -128,7 +116,7 @@ class CTCModel():
         cell = tf.contrib.rnn.GRUCell(Config.num_hidden)
         outputs, state = tf.nn.dynamic_rnn(cell, self.inputs_placeholder, self.seq_lens_placeholder, dtype=tf.float32)
         outputsShape = tf.shape(outputs)
-        W = tf.get_variable(name="W", shape=[Config.num_hidden, Config.num_classes], dtype=tf.float32, initializer=tf.contrib.layers.xavier_ini    tializer())
+        W = tf.get_variable(name="W", shape=[Config.num_hidden, Config.num_classes], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
         b = tf.get_variable(name="b", shape=[Config.num_classes], initializer=tf.zeros_initializer())
 
         # reshape to 2D
@@ -139,62 +127,22 @@ class CTCModel():
         logits = tf.reshape(logits, shape=[outputsShape[0], outputsShape[1], Config.num_classes])
 
         ### END YOUR CODE
-
+        self.last_hidden_state = state[-1] #i added this!!!- -> this is exactly what we need for determining funniness
         self.logits = logits
-
-
-    def add_loss_op(self):
-        """Adds Ops for the loss function to the computational graph. 
-
-        - Use tf.nn.ctc_loss to calculate the CTC loss for each example in the batch. You'll need self.logits,
-          self.targets_placeholder, self.seq_lens_placeholder for this. Set variable ctc_loss to
-          the output of tf.nn.ctc_loss
-        - You will need to first tf.transpose the data so that self.logits is shaped [max_timesteps, batch_s, 
-          num_classes]. 
-        - Configure tf.nn.ctc_loss so that identical consecutive labels are allowed
-        - Compute L2 regularization cost for all trainable variables. Use tf.nn.l2_loss(var). 
-
-        """
-        ctc_loss = []
-        l2_cost = 0.0
-
-        ### YOUR CODE HERE (~6-8 lines)
-        self.logits = tf.transpose(self.logits, perm=(1, 0, 2))
-        ctc_loss = tf.nn.ctc_loss(self.targets_placeholder, self.logits, self.seq_lens_placeholder, preprocess_collapse_repeated=False, ctc_mer    ge_repeated=False)
-        all_vars = tf.trainable_variables()
-        train_vars = []
-
-        #Don't include biases in L2 regularization
-        for v in train_vars:
-                if tf.shape(v) != 1:
-                        train_vars.append(v)
-        l2_cost += tf.nn.l2_loss(train_vars)
-        ### END YOUR CODE
-
-        # Remove inf cost training examples (no path found, yet)
-        loss_without_invalid_paths = tf.boolean_mask(ctc_loss, tf.less(ctc_loss, tf.constant(10000.)))
-        self.num_valid_examples = tf.cast(tf.shape(loss_without_invalid_paths)[0], tf.int32)
-        cost = tf.reduce_mean(loss_without_invalid_paths) 
-
-        self.loss = Config.l2_lambda * l2_cost + cost               
 
     def add_training_op(self):
         """Sets up the training Ops.
 
         Creates an optimizer and applies the gradients to all trainable variables. The Op returned by this
-        function is what must be passed to the `sess.run()` call to cause the model to train. See
+        function is what must be passed to the `sess.run()` call to cause the model to train. For more 
+        information, see:
 
         https://www.tensorflow.org/versions/r0.7/api_docs/python/train.html#Optimizer
-
-        for more information.
-
-        Use tf.train.AdamOptimizer for this model. Call optimizer.minimize() on self.loss. 
-
         """
         optimizer = None 
 
         ### YOUR CODE HERE (~1-2 lines)
-        optimizer = tf.train.AdamOptimizer(Config.lr).minimize(self.loss)
+        optimizer = tf.train.AdamOptimizer(Config.lr) #.minimize(self.loss)
         ### END YOUR CODE
         
         self.optimizer = optimizer
@@ -211,15 +159,15 @@ class CTCModel():
 
         ### YOUR CODE HERE (~2-3 lines)
         decoded_sequence = tf.nn.ctc_beam_search_decoder(self.logits, self.seq_lens_placeholder, merge_repeated=False)[0][0]
-        wer = tf.edit_distance(tf.cast(decoded_sequence, tf.int32), self.targets_placeholder, normalize=True)
-        wer = tf.reduce_mean(wer)
+        #wer = tf.edit_distance(tf.cast(decoded_sequence, tf.int32), self.targets_placeholder, normalize=True)
+        #wer = tf.reduce_mean(wer)
         ### END YOUR CODE
 
         tf.summary.scalar("loss", self.loss)
-        tf.summary.scalar("wer", wer)
+        #tf.summary.scalar("wer", wer)
 
         self.decoded_sequence = decoded_sequence
-        self.wer = wer
+        #self.wer = wer
 
     def add_summary_op(self):
         self.merged_summary_op = tf.summary.merge_all()
@@ -229,7 +177,6 @@ class CTCModel():
     def build(self):
         self.add_placeholders()
         self.add_prediction_op()
-        self.add_loss_op()
         self.add_training_op()       
         self.add_decoder_and_wer_op()
         self.add_summary_op()
@@ -237,14 +184,15 @@ class CTCModel():
 
     def train_on_batch(self, session, train_inputs_batch, train_targets_batch, train_seq_len_batch, train=True):
         feed = self.create_feed_dict(train_inputs_batch, train_targets_batch, train_seq_len_batch)
-        batch_cost, wer, batch_num_valid_ex, summary = session.run([self.loss, self.wer, self.num_valid_examples, self.merged_summary_op], feed)
+        batch_cost, batch_num_valid_ex, summary = session.run([self.loss, self.num_valid_examples, self.merged_summary_op], feed)
+        #took out wer and self.wer
 
         if math.isnan(batch_cost): # basically all examples in this batch have been skipped 
             return 0
         if train:
             _ = session.run([self.optimizer], feed)
 
-        return batch_cost, wer, summary
+        return batch_cost, summary
 
     def print_results(self, train_inputs_batch, train_targets_batch, train_seq_len_batch):
         train_feed = self.create_feed_dict(train_inputs_batch, train_targets_batch, train_seq_len_batch)
@@ -254,11 +202,11 @@ class CTCModel():
     def __init__(self):
         self.build()
 
-    
+#def get_features_from_rnn(): 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_path', nargs='?', default='./data/hw3_train.dat', type=str, help="Give path to training data - this should not need to be changed if you are running from the assignment directory")
-    parser.add_argument('--val_path', nargs='?', default='./data/hw3_val.dat', type=str, help="Give path to val data - this should not need to be changed if you are running from the assignment directory")
+    parser.add_argument('--train_path', nargs='?', default='./switchboardaudiosmall.train.pkl', type=str, help="Give path to training data")
+    parser.add_argument('--val_path', nargs='?', default='./switchboardaudiosmall.val.pkl', type=str, help="Give path to val data")
     parser.add_argument('--save_every', nargs='?', default=None, type=int, help="Save model every x iterations. Default is not saving at all.")
     parser.add_argument('--print_every', nargs='?', default=10, type=int, help="Print some training and val examples (true and predicted sequences) every x iterations. Default is 10")
     parser.add_argument('--save_to_file', nargs='?', default='saved_models/saved_model_epoch', type=str, help="Provide filename prefix for saving intermediate models")
@@ -305,27 +253,27 @@ if __name__ == "__main__":
             step_ii = 0
 
             for curr_epoch in range(Config.num_epochs):
-                total_train_cost = total_train_wer = 0
+                total_train_cost = 0#total_train_wer = 0
                 start = time.time()
 
                 for batch in random.sample(range(num_batches_per_epoch),num_batches_per_epoch):
                     cur_batch_size = len(train_seqlens_minibatches[batch])
 
-                    batch_cost, batch_ler, summary = model.train_on_batch(session, train_feature_minibatches[batch], train_labels_minibatches[batch], train_seqlens_minibatches[batch], train=True)
+                    batch_cost, summary = model.train_on_batch(session, train_feature_minibatches[batch], train_labels_minibatches[batch], train_seqlens_minibatches[batch], train=True)
                     total_train_cost += batch_cost * cur_batch_size
-                    total_train_wer += batch_ler * cur_batch_size
+                    #total_train_wer += batch_ler * cur_batch_size
                     
                     train_writer.add_summary(summary, step_ii)
                     step_ii += 1 
 
                     
                 train_cost = total_train_cost / num_examples
-                train_wer = total_train_wer / num_examples
+                #train_wer = total_train_wer / num_examples
 
-                val_batch_cost, val_batch_ler, _ = model.train_on_batch(session, val_feature_minibatches[0], val_labels_minibatches[0], val_seqlens_minibatches[0], train=False)
+                val_batch_cost, _ = model.train_on_batch(session, val_feature_minibatches[0], val_labels_minibatches[0], val_seqlens_minibatches[0], train=False)
                 
                 log = "Epoch {}/{}, train_cost = {:.3f}, train_ed = {:.3f}, val_cost = {:.3f}, val_ed = {:.3f}, time = {:.3f}"
-                print(log.format(curr_epoch+1, Config.num_epochs, train_cost, train_wer, val_batch_cost, val_batch_ler, time.time() - start))
+                print(log.format(curr_epoch+1, Config.num_epochs, train_cost, val_batch_cost, val_batch_ler, time.time() - start))
             
                 if args.print_every is not None and (curr_epoch + 1) % args.print_every == 0: 
                     batch_ii = 0

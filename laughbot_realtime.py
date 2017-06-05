7602dc97468b30401f90b6e3676df81becde3d6e
Natalie Muenster <ncm000@stanford.edu>
2017-06-04 21:19:26 -0700

    Google cloud API successfully called with credentials -- transcribes speech from microphone.

--- laughbot_realtime.py
+++ laughbot_realtime.py
@@ -3,7 +3,7 @@ import numpy as np
 #The Cloud Speech API v1 is officially released and is generally available from the https://speech.googleapis.com/v1/speech endpoint.
 #Should probably move the service_account_key file (CS224s-Laughbot-cdb7a14ba039.json) to local directories instead of shared for data safety and permissions reasons, after you login
 '''Run this:
-	export GOOGLE_APPLICATION_CREDENTIALS=./CS224s-Laughbot-cdb7a14ba039.json #set environment variable to key path
+	export GOOGLE_APPLICATION_CREDENTIALS=./service_account_key.json #set environment variable to key path
 	Follow instructions for Google cloud SDK installation: https://cloud.google.com/sdk/downloads 
 	https://cloud.google.com/sdk/docs/quickstarts
 	(to change compute zone: gcloud config set compute/zone NAME)
@@ -14,7 +14,7 @@ import numpy as np
 		"
 
 	gcloud auth application-default login (just hit y when it asks and it will redirect you to authenticate)
-	gcloud auth activate-service-account --key-file=./CS224s-Laughbot-cdb7a14ba039.json
+	gcloud auth activate-service-account --key-file=./service_account_key.json
 
 	Using speech API: (https://cloud.google.com/speech/docs/getting-started)
 	gcloud auth application-default print-access-token:
@@ -27,11 +27,41 @@ import numpy as np
 
     #You can also batch long audio files for speech recognition (using Asynchronous Recognition requests) or have the Speech API listen to a stream and return results while the stream is open using a gRPC StreamingRecognize request. For example, a streaming recognition task may provide transcription from a user while they are speaking. 
 
+	#https://pypi.python.org/pypi/SpeechRecognition/
+    pip install SpeechRecognition
+    Google API Client Library for Python (required only if you need to use the Google Cloud Speech API, recognizer_instance.recognize_google_cloud) pip install google-api-python-client
+    brew install portaudio && sudo brew link portaudio
+    pip install pyaudio
 '''
+
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something and laughbot will decide whether you're funny!")
+    audio = r.listen(source)
+
+# recognize speech using Google Cloud Speech
+#If credentials not specified, the library will try to automatically `find the default API credentials JSON file <https://developers.google.com/identity/protocols/application-default-credentials>`__.
+#used to be CS224s-Laughbot-cdb7a14ba039.json
+'''GOOGLE_CLOUD_SPEECH_CREDENTIALS = {}''' #If default credentials not working from the environment variable, insert credentials here and add to r.recognize_google_cloud call
+try:
+    print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio))#, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
+except sr.UnknownValueError:
+    print("Google Cloud Speech could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
+
+
+
+
 
 #The Cloud Speech API v1 is officially released and is generally available from the https://speech.googleapis.com/v1/speech endpoint
 
-from oauth2client.client import GoogleCredentials
+'''from oauth2client.client import GoogleCredentials
 credentials = GoogleCredentials.get_application_default()
 
 #https://cloud.google.com/docs/authentication
@@ -42,3 +72,27 @@ def create_service():
     # You can browse other available api services and versions here:
     #     https://developers.google.com/api-client-library/python/apis/
     return googleapiclient.discovery.build('storage', 'v1')
+
+
+
+#example code: https://cloud.google.com/speech/docs/streaming-recognize#speech-streaming-recognize-python
+def transcribe_streaming(stream_file):
+    """Streams transcription of the given audio file."""
+    from google.cloud import speech
+    speech_client = speech.Client()
+
+    with io.open(stream_file, 'rb') as audio_file:
+        audio_sample = speech_client.sample(
+            stream=audio_file,
+            encoding=speech.encoding.Encoding.LINEAR16,
+            sample_rate_hertz=16000)
+        alternatives = audio_sample.streaming_recognize('en-US')
+
+        for alternative in alternatives:
+            print('Finished: {}'.format(alternative.is_final))
+            print('Stability: {}'.format(alternative.stability))
+            print('Transcript: {}'.format(alternative.confidence))
+            print('Transcript: {}'.format(alternative.transcript))
+
+'''
+

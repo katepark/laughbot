b6097fb25f647f2254d92b8d91d1b831060f2430
Kate Park <kate.of.park@gmail.com>
2017-06-06 21:50:27 -0700

    laughbot

--- convertaudiosample.py
+++ convertaudiosample.py
@@ -4,13 +4,14 @@ import subprocess
 
 MAX_AUDIO_VECTOR = 50
 
-# TODO: call extract.sh here
-subprocess.call("./extract.sh", shell=True)
+def convert_audio_sample():
+	# TODO: call extract.sh here
+	subprocess.call("./extract.sh", shell=True)
 
-outFile = open('laughbot_audio.test.pkl', 'wb')
-mfccs = np.genfromtxt('laughbot_audio.csv', delimiter=',', skip_header=1)
-endMfcc = len(mfccs)
-startMfcc = max(0, endMfcc - MAX_AUDIO_VECTOR)
+	outFile = open('laughbot_audio.test.pkl', 'wb')
+	mfccs = np.genfromtxt('laughbot_audio.csv', delimiter=',', skip_header=1)
+	endMfcc = len(mfccs)
+	startMfcc = max(0, endMfcc - MAX_AUDIO_VECTOR)
 
-audio = mfccs[startMfcc:endMfcc, 2:]
-pickle.dump(([audio], [0], [len(audio)]), outFile)
+	audio = mfccs[startMfcc:endMfcc, 2:]
+	pickle.dump(([audio], [0], [len(audio)]), outFile)

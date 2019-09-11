039f069f3e8d73acedc608320710ef408b706a90
Annie <anniehu4@gmail.com>
2019-09-10 23:17:44 -0700

    various changes

--- laughbot_realtime.py
+++ laughbot_realtime.py
@@ -149,12 +149,12 @@ if __name__ == "__main__":
 		    # main REPL loop
 			response = raw_input("Press 's' to start: ")
 			while response != 'q':
-			    # print("press enter to stop recording")
-			    # record_audio()
-			    # print("audio recorded")
+			    print("press enter to stop recording")
+			    record_audio()
+			    print("audio recorded")
 			    transcript = get_transcript_from_file(credential)
 			    print("transcript: ", transcript)
-			    # convert_audio_sample()
+			    convert_audio_sample()
 			    
 			    test_dataset = load_dataset("laughbot_audio.test.pkl")
 			    feature_b, label_b, seqlens_b = make_batches(test_dataset, batch_size=len(test_dataset[0]))
